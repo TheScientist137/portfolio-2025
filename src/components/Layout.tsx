676b8be3f020823fef913726dcf023ec6dc58002
The-Scientist <thescientist137@outlook.es>
2025-02-06 13:17:09 +0100

    Created portfolio pages and it's routes in router.tsx -- first retro styles introduced

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,20 @@
+import { Link, Outlet } from "react-router";
+
 export default function Layout() {
  return (
-  <div>
-   <p>layout</p>
+  // introducir retro font
+  <div className="min-h-screen bg-retroBlack text-retroGreen">
+   <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/projects'>Projects</Link>
+    <Link to='/contact'>Contact</Link>
+   </nav>
+
+   <main>
+    <Outlet />
+   </main>
+
+   <footer>This is a footer</footer>
   </div>
  )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
  {
   path: '/',
-  element: <Layout />
+  element: <Layout />,
+  children: [
+   { index: true, element: <Home /> },
+   { path: '/projects', element: <Projects /> },
+   { path: '/contact', element: <Contact /> }
+  ]
  }
 ]);
 
